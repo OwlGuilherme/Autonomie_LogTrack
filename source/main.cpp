89320e5e64dad8fb11b3434d8103d7021833978a
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-25 23:15:19 -0300

    Simplicado arquivo main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,23 +1,9 @@
 #include "funcs.hpp"
-#include <iostream>
-#include <iomanip>
-
 
 
 int main() {
 
-    float km_inicial, km_final, volume_abastecido;
-
-    std::cout << "Digite a quilometragem inicial: ";
-    std::cin >> km_inicial;
-
-    std::cout << "Digite a quilometragem final: ";
-    std::cin >> km_final;
-
-    std::cout << "Digite o volume abastecido: ";
-    std::cin >> volume_abastecido;
-
-    std::cout << "Autonomia: " << std::setprecision(3) << autonomy(km_inicial, km_final, volume_abastecido) << "km/l" << '\n';
+    menu();    
 
     return 0;
 }
