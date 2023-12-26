6043610e9d6b0fcc5fef68cb50e490fb34d10b50
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-25 22:05:19 -0300

    Corrigido nome de uma variável

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <iomanip>
 
-float autonomy(float km_inicial, float km_final, float volume_abastacido) {
+float autonomy(float km_inicial, float km_final, float volume_abastecido) {
     auto distancia = km_final - km_inicial;
-    float autonomia = distancia / volume_abastacido;
+    float autonomia = distancia / volume_abastecido;
 
     return autonomia;
 }
@@ -11,7 +11,7 @@ float autonomy(float km_inicial, float km_final, float volume_abastacido) {
 
 int main() {
 
-    float km_inicial, km_final, volume_abastacido;
+    float km_inicial, km_final, volume_abastecido;
 
     std::cout << "Digite a quilometragem inicial: ";
     std::cin >> km_inicial;
@@ -20,9 +20,9 @@ int main() {
     std::cin >> km_final;
 
     std::cout << "Digite o volume abastecido: ";
-    std::cin >> volume_abastacido;
+    std::cin >> volume_abastecido;
 
-    std::cout << std::setprecision(3) << autonomy(km_inicial, km_final, volume_abastacido) << "km/l" << '\n';
+    std::cout << std::setprecision(3) << autonomy(km_inicial, km_final, volume_abastecido) << "km/l" << '\n';
 
     return 0;
 }
