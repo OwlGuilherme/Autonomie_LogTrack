fbe9ee8d667a0b55a9d78fd0a4d311ce3a75ad92
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-25 21:38:57 -0300

    Implementada função para calcular autonimia e trecho de testes

--- main.cpp
+++ main.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 
-struct Autonomy {
-    int km_inicial;
-    int km_final;
-    float volume_abastacido;
+float autonomy(float km_inicial, float km_final, float volume_abastacido) {
+    auto distancia = km_final - km_final;
+    float autonomia = distancia / volume_abastacido;
 
-    void input_data(int st_km_inicial, int st_km_final, float st_volume_abastacido){    
-        km_inicial = st_km_inicial;
-        km_final = st_km_final;
-        volume_abastacido = st_volume_abastacido;
-    }
-};
+    return autonomia;
+}
+
 
 int main() {
+
+    float km_inicial, km_final, volume_abastacido;
+
+    std::cout << "Digite a quilometragem inicial: ";
+    std::cin >> km_inicial;
+
+    std::cout << "Digite a quilometragem final: ";
+    std::cin >> km_final;
+
+    std::cout << "Digite o volume abastecido: ";
+    std::cin >> volume_abastacido;
+
+    std::cout << "Km inicial: " << km_inicial << "\n";
+    std::cout << "Km final: " << km_final << "\n";
+    std::cout << "Volume abastecido: " << volume_abastacido << "\n";
 
     return 0;
 }
