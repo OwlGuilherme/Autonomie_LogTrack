4e0efea25d2a620f1d48491cd0b40b2cff7a89d9
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-25 21:05:36 -0300

    Criada estrutura e método para autonomia

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+struct Autonomy {
+    int km_inicial;
+    int km_final;
+    float volume_abastacido;
+
+    void input_data(int st_km_inicial, int st_km_final, float st_volume_abastacido){    
+        km_inicial = st_km_inicial;
+        km_final = st_km_final;
+        volume_abastacido = st_volume_abastacido;
+    }
+};
+
 int main() {
 
     return 0;
