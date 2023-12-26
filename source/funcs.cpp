84b1b23b61e1a9c33b281efc09f3cdf9cfa179ef
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-12-25 23:13:17 -0300

    Implementada função para apresentar menu na tela

--- source/funcs.cpp
+++ source/funcs.cpp
@@ -8,3 +8,17 @@ float autonomy(float km_inicial, float km_final, float volume_abastecido) {
 
     return autonomia;
 }
+
+void menu(){
+    
+    float km_inicial, km_final, volume_abastecido;
+
+    std::cout << "Digite a quilometragem inicial: ";
+    std::cin >> km_inicial;
+
+    std::cout << "Digite a quilometragem final: ";
+    std::cin >> km_final;
+
+    std::cout << "Digite o volume abastecido: ";
+    std::cin >> volume_abastecido;
+}
--- source/funcs.hpp
+++ source/funcs.hpp
@@ -1,3 +1,5 @@
 #pragma once
 
 float autonomy(float km_inicial, float km_final, float volume_abastecido);
+
+void menu();
